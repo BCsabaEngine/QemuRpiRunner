6e5213e649522dac03cf2410da8edb74647c3600
Balázs Csaba <csaba@balazs-magyar.hu>
2021-09-30 22:06:34 +0200

    Faster qemu

--- 3.build-qemu.sh
+++ 3.build-qemu.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ./qemu
 
-git clone https://github.com/qemu/qemu ./qemu
+git clone --depth 1  https://github.com/qemu/qemu ./qemu
 patch ./qemu/hw/usb/dev-network.c ./src/qemu/hw/usb/dev-network.patch
 cd ./qemu
 ./configure --target-list=aarch64-softmmu
-make -j4
+make -j$(nproc)
