9f98b5e82dda6a9398a94d509212166374f6fba6
Balázs Csaba <csaba@balazs-magyar.hu>
2021-09-30 10:51:44 +0200

    Patch qemu to hide invalid transactions

--- 3.build-qemu.sh
+++ 3.build-qemu.sh
@@ -3,6 +3,7 @@
 mkdir -p ./qemu
 
 git clone https://github.com/qemu/qemu ./qemu
+patch ./qemu/hw/usb/dev-network.c ./src/qemu/hw/usb/dev-network.patch
 cd ./qemu
 ./configure --target-list=aarch64-softmmu
 make -j4
